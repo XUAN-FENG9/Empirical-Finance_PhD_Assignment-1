771904bc768ca894f39c31bcfedb436a3a478dc5
georgewu1 <57053044+georgewu1@users.noreply.github.com>
2023-05-29 23:11:44 +0800

    Add files via upload

--- empirical.R
+++ empirical.R
@@ -4,7 +4,7 @@ library(readxl)
 file_path <- "C:/Users/George Wu/Documents/Empirical-Finance_PhD_Assignment-1-main/chapt26.xlsx"
 library(dplyr)
 df <- read_excel(file_path, sheet = "Data")
-df <- slice(df, 11:254)
+df <- slice(df, 9:254)
 df <- select(df, 1,2,5,7, 8, 9, 16)
 colnames(df)<-c('year','SPCSPI','IR', "CPI", "Real_1yr",'real_pc_consumption', "real_SP_return")
 df$year<-as.numeric(df$year)
@@ -17,9 +17,13 @@ df$real_SP_return<-as.numeric(df$real_SP_return)
 
 Shiller <- na.omit(df)
 
-# Read CSV file of Fama-French data - annual return begins from row 2240
+
+
+write.csv(Shiller, "C:/Users/George Wu/Documents/Empirical-Finance_PhD_Assignment-1-main/spx_data.csv")
+
+#Read CSV file of Fama-French data - annual return begins from row 2240
 FF <- read.csv("C:/Users/George Wu/Documents/Empirical-Finance_PhD_Assignment-1-main/5_Industry_Portfolios.csv", header = TRUE, skip = 2439, nrows = 96, colClasses=c('numeric','numeric','numeric','numeric','numeric','numeric'))
 FF<-rename(FF,'year'=X)
 #Merge the two database and delete NA value rows
 data=merge(FF,Shiller,by='year')
-
+write.csv(data, "C:/Users/George Wu/Documents/Empirical-Finance_PhD_Assignment-1-main/industry_data.csv")
